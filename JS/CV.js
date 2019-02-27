aaeedaf9ceb49268cdc70462363ef74c4b201a11
romain Pommier <romain-p31@hotmail.fr>
2019-02-27 15:36:32 +0100

    mise en ligne

--- JS/CV.js
+++ JS/CV.js
@@ -1,107 +1,130 @@
-$(function(){
-    
-    $(".navbar a, footer a").on("click", function(event){
-       event.preventDefault();
+$(function () {
+
+    $(".navbar a, footer a").on("click", function (event) {
+        event.preventDefault();
         var hash = this.hash;
-        
-        $('body,html').animate({scrollTop: $(hash).offset().top} , 900 , function(){window.location.hash = hash;})
-    });  
+
+        $('body,html').animate({ scrollTop: $(hash).offset().top }, 900, function () { window.location.hash = hash; })
+    });
 })
-$(".input").focus(function() {
+$(".input").focus(function () {
     $(this).parent().addClass("focus");
 })
 $(function () {
 
     'use strict';
-            // error variables
-            var UserError  = true,
-                EmailError = true,
-                SubError   = true,
-                MsgError   = true;
-    
-    $(".username").blur(function() {
-        
-        if($(this).val().length < 4){
-    
-            $(this).css('border','1px solid #F00');
+    // error variables
+    var UserError = true,
+        EmailError = true,
+        SubError = true,
+        MsgError = true;
+
+    $(".username").blur(function () {
+
+        if ($(this).val().length < 4) {
+
+            $(this).css('border', '1px solid #F00');
             $(this).parent().find('.custom-alert').fadeIn(300).end().find('.asterix').fadeOut(300).end().find('span.cross').fadeIn(300).end().find('span.verify').fadeOut(300);
             UserError = true;
-        }else{
-    
-            $(this).css('border','1px solid #080');
+        } else {
+
+            $(this).css('border', '1px solid #080');
             $(this).parent().find('.custom-alert').fadeOut(300).end().find('.asterix').fadeOut(300).end().find('span.verify').fadeIn(300).end().find('span.cross').fadeOut(300);
-    
+
             UserError = false;
         }
     });
-    
-    
-    
-    $(".email").blur(function() {
-        
-        if($(this).val() === ''){
-    
-            $(this).css('border','1px solid #F00');
+
+
+
+    $(".email").blur(function () {
+
+        if ($(this).val() === '') {
+
+            $(this).css('border', '1px solid #F00');
             $(this).parent().find('.custom-alert').fadeIn(300).end().find('.asterix').fadeOut(300).end().find('span.cross').fadeIn(300).end().find('span.verify').fadeOut(300);
             EmailError = true;
-        }else{
-    
-            $(this).css('border','1px solid #080');
+        } else {
+
+            $(this).css('border', '1px solid #080');
             $(this).parent().find('.custom-alert').fadeOut(300).end().find('.asterix').fadeOut(300).end().find('span.verify').fadeIn(300).end().find('span.cross').fadeOut(300);
-    
+
             EmailError = false;
         }
     });
-    
-    
-    $(".subject").blur(function() {
-        
-        if($(this).val() === ''){
-    
-            $(this).css('border','1px solid #F00');
+
+
+    $(".subject").blur(function () {
+
+        if ($(this).val() === '') {
+
+            $(this).css('border', '1px solid #F00');
             $(this).parent().find('.custom-alert').fadeIn(300).end().find('.asterix').fadeOut(300).end().find('span.cross').fadeIn(300).end().find('span.verify').fadeOut(300);
             SubError = true;
-        }else{
-    
-            
-            $(this).css('border','1px solid #080');
+        } else {
+
+
+            $(this).css('border', '1px solid #080');
             $(this).parent().find('.custom-alert').fadeOut(300).end().find('.asterix').fadeOut(300).end().find('span.verify').fadeIn(300).end().find('span.cross').fadeOut(300);
-    
+
             SubError = false;
         }
     });
-    
-    
-    $(".message").blur(function() {
-        
-        if($(this).val() .length < 10){
-    
-            
-            $(this).css('border','1px solid #F00');
+
+
+    $(".message").blur(function () {
+
+        if ($(this).val().length < 15) {
+
+
+            $(this).css('border', '1px solid #F00');
             $(this).parent().find('.custom-alert').fadeIn(300).end().find('.asterix').fadeOut(300).end().find('span.cross').fadeIn(300).end().find('span.verify').fadeOut(300);
             MsgError = true;
-        }else{
-    
-            $(this).css('border','1px solid #080');
+        } else {
+
+            $(this).css('border', '1px solid #080');
             $(this).parent().find('.custom-alert').fadeOut(300).end().find('.asterix').fadeOut(300).end().find('span.verify').fadeIn(300).end().find('span.cross').fadeOut(300);
             MsgError = false;
         }
     });
-    
-    
-    // submit form
-    
-    $('.contact-form').submit(function(event) {
-         
-         if(UserError === true || EmailError === true || SubError === true || MsgError === true){
-    
-             event.preventDefault(); // prevent sending 
-             $('.username,.email,.subject,.message').blur();
-         }
-    
-    
-    });
-    
-    });
 
- 
+
+    // submit form
+
+    // $('.contact-form').submit(function(event) {
+
+    //      if(UserError === true || EmailError === true || SubError === true || MsgError === true){
+
+    //          event.preventDefault(); // prevent sending 
+    //          $('.username,.email,.subject,.message').blur();
+    //      }
+
+
+    // });
+});
+
+
+
+let uri = document.URL;
+if (uri.includes("sent=")) {
+    let index = uri.indexOf("=");
+    let sent = +uri.substr(index, 1);
+
+    let popup = document.getElementById("popupMail");
+    let infos = document.getElementById("popupMailInfos");
+
+    if (sent == 1) {
+        popup.style.backgroundColor = "green";
+        infos.textContent = "Mail envoyé !";
+    }
+    else if (sent == 0) {
+        popup.style.backgroundColor = "red";
+        infos.textContent = "Une erreur c'est produite pendant l'envoie du mail.";
+    }
+
+    popup.style.display = "block";
+
+    setTimeout(() => {
+        popup.style.display = 'none';
+    }, 3000);
+}
--- php/sendMail.php
+++ php/sendMail.php
@@ -1,8 +1,11 @@
 <?php
-
+//echo phpinfo();
 // My email
 $siteOwnersEmail = 'romain-p31@hotmail.fr';
-
+$host  = $_SERVER['HTTP_HOST'];
+$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+$dir = dirname($uri);
+$file = 'index.html';
 
 if($_POST) {
 
@@ -10,6 +13,7 @@ if($_POST) {
    $email = trim(stripslashes($_POST['contactEmail']));
    $subject = trim(stripslashes($_POST['contactSubject']));
    $contact_message = trim(stripslashes($_POST['contactMessage']));
+	$error = "";
 
    // Check Name
 	if (strlen($name) < 2) {
@@ -24,10 +28,10 @@ if($_POST) {
 		$error['message'] = "Votre message doit avoir minimum 15 caractères.";
 	}
    // Subject
-	if ($subject == '') { $subject = "L'objet du message."; }
+	if ($subject == '') { $subject = "portfolio email"; }
 
-
-   // Set Message
+	// Set Message
+	$message = "";
    $message .= "Email from: " . $name . "<br />";
 	$message .= "Email address: " . $email . "<br />";
    $message .= "Message: <br />";
@@ -43,27 +47,25 @@ if($_POST) {
  	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-
    if (!$error) {
-
-      ini_set("sendmail_from", $siteOwnersEmail); // for windows server
+       ini_set("sendmail_from", $siteOwnersEmail); // for windows server
       $mail = mail($siteOwnersEmail, $subject, $message, $headers);
 
-		if ($mail) { echo "OK"; }
+		if ($mail) {
+			header("Location: http://$host$dir/$file" . "?sent=1");
+		}
       else { echo "Votre message n'a été envoyé. Ressayez s'il vous plaît."; }
 		
 	} # end if - no validation error
 
 	else {
-
-		$response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
-		$response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
-		$response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
-		
-		echo $response;
+		header("Location: http://$host$dir/$file" . "?sent=0");
 
 	} # end if - there was a validation error
 
+}
+else {
+	header("Location: http://$host$dir/$file");
 }
 
 ?>
