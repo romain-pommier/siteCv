c2bbb0ba780fa93c831d9f3456ac78f730be5790
romain-pommier <romain-p31@hottmail.fr>
2020-02-18 16:26:03 +0100

    ajout sitemape

---
+++ public/sitemap.php
@@ -0,0 +1,10 @@
+<?php header('Content-type: text/xml'); ?>
+<?xml version="1.0" encoding="utf-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>http://romain-pommier.com/</loc>
+        <lastmod>2020-02-18</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>
+</urlset>
