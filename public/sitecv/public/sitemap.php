510d892dc925e943cf000b0273c964fef862cd6c
romain pommier <romain-p31@hotmail.fr>
2020-10-15 10:31:43 +0200

    update picture/ skills/ css nav-bar

--- public/sitecv/public/sitemap.php
+++
@@ -1,9 +0,0 @@
-<?php header('Content-type: text/xml'); ?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-        <loc>http://romain-pommier.com/</loc>
-        <lastmod>2020-02-18</lastmod>
-        <changefreq>daily</changefreq>
-        <priority>1.0</priority>
-    </url>
-</urlset>
